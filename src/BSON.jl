c0b8797235cc85302058315a5f09d9d01e7bdb99
Mike J Innes <mike.j.innes@gmail.com>
2018-02-27 17:19:30 +0000

    utility macros

--- src/BSON.jl
+++ src/BSON.jl
@@ -32,4 +32,20 @@ include("read.jl")
 include("extensions.jl")
 include("anonymous.jl")
 
+macro save(file, ks...)
+  @assert all(k -> k isa Symbol, ks)
+  ss = Expr.(:quote, ks)
+  :(bson($(esc(file)), Dict($([:($s=>$(esc(k))) for (s,k) in zip(ss,ks)]...) )))
+end
+
+macro load(file, ks...)
+  @assert all(k -> k isa Symbol, ks)
+  ss = Expr.(:quote, ks)
+  quote
+    data = load($(esc(file)))
+    ($(esc.(ks)...),) = ($([:(data[$k]) for k in ss]...),)
+    nothing
+  end
+end
+
 end # module
