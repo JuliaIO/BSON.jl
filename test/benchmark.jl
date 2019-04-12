1bd75ef418483053125398119d0423b572e38784
Richard Palethorpe <rpalethorpe@suse.com>
2019-04-12 10:51:41 +0200

    Increase benchmarking

--- test/benchmark.jl
+++ test/benchmark.jl
@@ -30,65 +30,78 @@ struct Foo
   projects::Vector{Bar}
 end
 
-Foo() = Foo(rstr(5), rstr(7), rstr(17), rstr(11), rstr(13), [Bar() for _ in 1:2000])
+Foo() = [Foo(rstr(5), rstr(7), rstr(17), rstr(11), rstr(13),
+             [Bar() for _ in 1:2000]) for _ in 1:5]
+
+struct Result
+  elapsed::Float64
+  allocated::Int64
+end
 
 const history_file = "./benchmark-history.bson"
 history = if isfile(history_file)
-    BSON.load(history_file)
+    BSON.load(history_file)::Dict{String, Result}
 else
-    Dict()
+    Dict{String, Result}()
 end
 
 macro bench(msg, ex)
-    sex = "$ex"
-    quote
-        GC.gc()
-        @info $msg
-        local val, t1, bytes, gctime, memallocs = @timed $(esc(ex))
-        kb = bytes / 1024
-        if $sex in keys(history)
-            @info $sex elapsed=t1 speedup=history[$sex]/t1 allocatedKb=bytes gctime
-        else
-            @info $sex elapsed=t1 allocatedKb=bytes gctime
-        end
-        history[$sex] = t1
-        val
+  sex = "$ex"
+  quote
+    GC.gc()
+    @info $msg
+    local val, t1, bytes, gctime, memallocs = @timed $(esc(ex))
+    local mb = ceil(bytes / (1024 * 1024))
+    if $sex in keys(history)
+      local t0 = history[$sex].elapsed
+      @info $sex elapsed=t1 speedup=t0/t1 allocatedMb=mb gctime
+    else
+      @info $sex elapsed=t1 allocatedMb=mb gctime
     end
+    history[$sex] = Result(t1, bytes)
+    val
+  end
 end
 
 foos = Dict(:Foo => Foo(), :Foo2 => Foo(), :Foo3 => Foo())
+
+@bench "Roundtrip from cold start (ignore)" BSON.roundtrip(foos)
+
 io = IOBuffer()
 
 @bench "Bench Save BSON" bson(io, foos)
 seek(io, 0)
 
 dict = @bench "Bench Parse BSON" BSON.parse(io)
-@bench "Bench Raise BSON to Julia types" BSON.raise_recursive(dict)
+rfoos = @bench "Bench Raise BSON to Julia types" BSON.raise_recursive(dict)
 
 bson(history_file, history)
 
-GC.gc()
-@info "Profile Save BSON"
-@profile bson(io, foos)
-Profile.print(;noisefloor=2)
-Profile.clear()
-seek(io, 0)
+if get(ENV, "JULIA_PROFILE", nothing) != nothing
+  seek(io, 0)
+  GC.gc()
+  @info "Profile Save BSON"
+  @profile bson(io, foos)
+  Profile.print(;noisefloor=2)
+  Profile.clear()
+  seek(io, 0)
 
-GC.gc()
-@info "Profile Parse BSON"
-dict = @profile BSON.parse(io)
-Profile.print(;noisefloor=2, C=true)
-Profile.clear()
+  GC.gc()
+  @info "Profile Parse BSON"
+  dict = @profile BSON.parse(io)
+  Profile.print(;noisefloor=2, C=true)
+  Profile.clear()
 
-GC.gc()
-@info "Profile Raise BSON to Julia types"
-rfoos = @profile BSON.raise_recursive(dict)
-Profile.print(;noisefloor=2)
-Profile.clear()
+  GC.gc()
+  @info "Profile Raise BSON to Julia types"
+  rfoos = @profile BSON.raise_recursive(dict)
+  Profile.print(;noisefloor=2)
+  Profile.clear()
+end
 
 # Sanity check the results
-rfoos[:Foo]::Foo
-rfoos[:Foo].projects[1]::Bar
-rfoos[:Foo].projects[1].bazes[1]::Baz
+rfoos[:Foo][1]::Foo
+rfoos[:Foo][1].projects[1]::Bar
+rfoos[:Foo][1].projects[1].bazes[1]::Baz
 
 end
